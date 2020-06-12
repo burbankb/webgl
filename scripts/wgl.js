0ccab6ebf314ef719b90aa4cca73625c1425cbce
Brandyn Burbank <brandyn@brandynburbank.com>
2020-06-12 13:46:25 -0400

    spinning triangle

--- scripts/wgl.js
+++ scripts/wgl.js
@@ -1,7 +1,7 @@
 var webgl_init = function(context, vs_txt, fs_txt) {
     console.log("WEBGL:START")
 
-    //var gl:WebGLRenderingContext = context;
+    //var gl:WebGLRenderingContext = context; //for code hinting
     var gl = context;
     //WebGL START ---
 
@@ -39,10 +39,10 @@ var webgl_init = function(context, vs_txt, fs_txt) {
     //GEOM Start
 
     var verts = new Float32Array(
-    [ // X,Y          R,G,B
-        0.0, 0.5,     1, 0, 0,
-        -0.5, -0.5,   0, 1, 0,
-        0.5, -0.5,    0, 0, 1
+    [ // X,Y,Z             R,G,B
+         0.0,  0.5, 0.0,   1, 0, 0,
+        -0.5, -0.5, 0.0,   0, 1, 0,
+         0.5, -0.5, 0.0,   0, 0, 1
     ]);
     
 
@@ -53,10 +53,10 @@ var webgl_init = function(context, vs_txt, fs_txt) {
     var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     gl.vertexAttribPointer(
         positionAttribLocation,
-        2,
+        3,
         gl.FLOAT,
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT,
+        6 * Float32Array.BYTES_PER_ELEMENT,
         0
     );
 
@@ -66,17 +66,50 @@ var webgl_init = function(context, vs_txt, fs_txt) {
         3,
         gl.FLOAT,
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT,
-        2 * Float32Array.BYTES_PER_ELEMENT
+        6 * Float32Array.BYTES_PER_ELEMENT,
+        3 * Float32Array.BYTES_PER_ELEMENT
     );
 
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
 
-
-    /////// RENDER LOOP ///////
     gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+    var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+    var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+    var worldMatrix = new Float32Array(16);
+    var viewMatrix = new Float32Array(16);
+    var projMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(worldMatrix);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -2], [0, 0, 0], [0, 1, 0]);
+    glMatrix.mat4.perspective(projMatrix, 45 * (Math.PI/180), 8/6, 0.1, 1000.0);
+
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+
+    
+    var identityMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(identityMatrix);
+    var theta;
+    /////// RENDER LOOP ///////
+    var renderLoop = function () {
+        theta = performance.now() / 1000 / 6 * 2 * Math.PI;
+
+        glMatrix.mat4.rotate(worldMatrix, identityMatrix, theta, [0,1,0])
+
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix)
+
+        gl.clearColor(.7, .7, 1, 1.0)
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+      
+        requestAnimationFrame(renderLoop);
+    }
+    requestAnimationFrame(renderLoop);
 
 
 
